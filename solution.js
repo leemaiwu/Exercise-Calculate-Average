ed4dd940a5ff9d57a46cc05ce9ed44d348ea23b3
Laura Wu <leemaiwu@gmail.com>
2023-06-23 09:14:08 -0600

    Completed with alternate one line solution

--- solution.js
+++ solution.js
@@ -1,11 +1,19 @@
 function findAverage(array) {
-  let total = array.reduce((a, b) => a + b)
   if (array.length === 0) {
     return 0
   } else {
-    return total / array.length
+    return (array.reduce((a, b) => a + b) / array.length)
   }
 }
 
+console.log(findAverage([])) // 0
 console.log(findAverage([1,1,1])) // 1
 console.log(findAverage([1,2,3,4])) // 2.5
+
+// Alternate Solution
+
+const average = (arr) => !arr.length ? 0 : (arr.reduce((a, b) => a + b)) / arr.length
+  
+console.log(average([])) // 0
+console.log(average([1,1,1])) // 1
+console.log(average([1,2,3,4])) // 2.5
